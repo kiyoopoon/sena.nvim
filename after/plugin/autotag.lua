3e5687ccb75e4a642bf499a5f270ec20ab67ae3a
kiyoopoon <r932059@gmail.com>
2024-10-05 23:07:12 +0600

    fixed discord rpc and autoparis

--- after/plugin/autotag.lua
+++ after/plugin/autotag.lua
@@ -1,18 +1,15 @@
-require('nvim-ts-autotag').setup({
+require("nvim-ts-autotag").setup({
     opts = {
         enable_close = true,
         enable_rename = true,
-        enable_close_on_slash = false
+        enable_close_on_slash = false,
     },
 
-    per_filetype = { ["html"] = { enable_close = false } }
+    per_filetype = { ["html"] = { enable_close = false } },
 })
 
 -- Autopair
--- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
--- local cmp = require('cmp')
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
 
--- cmp.event:on(
---     'confirm_done',
---     cmp_autopairs.on_confirm_done()
--- )
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
--- lua/sena/keybinds.lua
+++ lua/sena/keybinds.lua
@@ -1,19 +1,20 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- Window focusing
-vim.api.nvim_set_keymap('n', '<A-h>', '<C-w>h', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-j>', '<C-w>j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-k>', '<C-w>k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-l>', '<C-w>l', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-h>", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-j>", "<C-w>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-k>", "<C-w>k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-l>", "<C-w>l", { noremap = true, silent = true })
 
 -- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fw', function()
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+vim.keymap.set("n", "<leader>fw", function()
     builtin.grep_string()
 end)
 
+-- LSP keybinds are in the lsp_config.lua file.
 -- Gitsigns keybinds are in the gitsigns.lua file.
 
 -- Nvimtree
@@ -21,27 +22,22 @@ vim.api.nvim_set_keymap("n", "<A-f>", "<cmd>Neotree focus<cr>", { noremap = true
 vim.api.nvim_set_keymap("n", "<A-e>", "<cmd>Neotree toggle<cr>", { noremap = true, silent = true })
 
 -- Bufferline
-vim.api.nvim_set_keymap('n', '<A-S-Tab>', ':BufferMovePrevious<CR>',
-    { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-Tab>', ':BufferMoveNext<CR>',
-    { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-n>', ':BufferNext<CR>',
-    { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-p>', ':BufferPrevious<CR>',
-    { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>c', ':BufferClose<CR>',
-    { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-p>', ':BufferPin<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-S-Tab>", ":BufferMovePrevious<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-Tab>", ":BufferMoveNext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-n>", ":BufferNext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-p>", ":BufferPrevious<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>c", ":BufferClose<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-p>", ":BufferPin<CR>", { noremap = true, silent = true })
 
 -- Undotree
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- Comment
-local comment = require('Comment')
-comment.setup {
-    toggler = { line = '<leader>cc', block = '<leader>cu' },
-    opleader = { line = '<leader>cc', block = '<leader>cu' }
-}
+local comment = require("Comment")
+comment.setup({
+    toggler = { line = "<leader>cc", block = "<leader>cu" },
+    opleader = { line = "<leader>cc", block = "<leader>cu" },
+})
 
 -- Color Picker
 vim.keymap.set("n", "<leader>w", "<cmd>PickColor<cr>")
