7fe006d51aa72f180c4a6d2bfddac1a5c6837b98
kiyoopoon <r932059@gmail.com>
2024-10-07 20:46:46 +0600

    fixed live greping [ word search ]

--- lua/sena/keybinds.lua
+++ lua/sena/keybinds.lua
@@ -11,7 +11,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 vim.keymap.set("n", "<leader>fw", function()
-    builtin.grep_string()
+    builtin.grep_string({ search = vim.fn.input("Search: ") })
 end)
 
 -- LSP keybinds are in the lsp_config.lua file.
