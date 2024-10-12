baaa383d473cb416b57ee7e3d4bc2663eae9b04c
kiyoopoon <r932059@gmail.com>
2024-10-12 19:21:56 +0600

    added image viewing support [ !! only for kitty terminal !! ]

---
+++ after/plugin/image.lua
@@ -0,0 +1,34 @@
+require("image").setup({
+    backend = "kitty",
+    integrations = {
+        markdown = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = false,
+            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        },
+        neorg = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = false,
+            filetypes = { "norg" },
+        },
+        html = {
+            enabled = false,
+        },
+        css = {
+            enabled = false,
+        },
+    },
+    max_width = nil,
+    max_height = nil,
+    max_width_window_percentage = nil,
+    max_height_window_percentage = 50,
+    window_overlap_clear_enabled = false,
+    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    editor_only_render_when_focused = false,
+    tmux_show_only_in_active_window = false,
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- We are gona hijack the files :skull:
+})
