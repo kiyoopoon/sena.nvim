0b8546e6fdcba12f8a6b787abf299cb72337db90
kiyoopoon <r932059@gmail.com>
2024-10-23 21:32:09 +0600

    added all themes [ will add more later ]

--- after/plugin/themery.lua
+++ after/plugin/themery.lua
@@ -9,14 +9,40 @@ require("themery").setup({
             colorscheme = "ayu-dark",
         },
         {
-
             name = "Kanagawa",
             colorscheme = "kanagawa",
         },
         {
-
             name = "Catppuccin",
             colorscheme = "catppuccin",
+        },
+        {
+            name = "Base16",
+            colorscheme = "base16-default-dark", -- Change it with your favorite base16 theme
+        },
+        {
+            name = "Ayu Light",
+            colorscheme = "ayu-light",
+        },
+        {
+            name = "Ayu Mirage",
+            colorscheme = "ayu-mirage",
+        },
+        {
+            name = "Kanagawa Wave",
+            colorscheme = "kanagawa-wave",
+        },
+        {
+            name = "Kanagawa Dragon",
+            colorscheme = "kanagawa-dragon",
+        },
+        {
+            name = "Rosé Pine Dawn",
+            colorscheme = "rose-pine-dawn",
+        },
+        {
+            name = "Rosé Pine Moon",
+            colorscheme = "rose-pine-moon",
         },
     },
     livePreview = true,
